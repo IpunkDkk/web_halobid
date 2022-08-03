b16531e9444824fd70a191af5b9d626d4a3b9c82
IpunkDkk <ipunka37@gmail.com>
2022-08-03 22:52:19 +0700

    try css

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>halobid</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="css/app.css" rel="stylesheet">
     <link rel="icon" sizes="16x16" href="dist/image/favicon/favicon.png">
 
 </head>
